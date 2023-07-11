fd8682355b1a6d26c5b0f782cc6ab03f50029383
Sarah Geerling <tehsarah@gmail.com>
2023-07-11 10:08:50 -0500

    part 1 of assignment completed

--- part1/exercises/src/components/BookList.js
+++ part1/exercises/src/components/BookList.js
@@ -1,15 +1,17 @@
 export default function BookList() {
-   let pageTitle = "shrug";
-   let book1 = "google.com";
-   let book2 = "";
-   let book3 = "";
+   let pageTitle = "Summer Reading List";
+   let book1 = "https://m.media-amazon.com/images/I/51LE-uEAZ9L._SY445_SX342_.jpg";
+   let book2 = "https://m.media-amazon.com/images/I/51-rLlDOGpL._SY445_SX342_.jpg";
+   let book3 = "https://m.media-amazon.com/images/I/51QuzhN2lTL._SY445_SX342_.jpg";
+   let book4 = "https://m.media-amazon.com/images/I/41iTM8LkxSL._SY445_SX342_.jpg";
 
    return (
       <div>
          <h3>{pageTitle}</h3>
-         <img src={book1} alt="Nothing here!" />
-         <img src={book2} alt="Nothing here!" />
-         <img src={book3} alt="Nothing here!" />
+         <img src={book1} alt="Lord of the Flies" />
+         <img src={book2} alt="The Secret Life of Bees" />
+         <img src={book3} alt="Purple Hibiscus" />
+         <img src={book4} alt="Educated" />
       </div>      
    );
 }
