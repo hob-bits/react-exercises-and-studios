6bf28854ccbf9ace6a99e55938cb528174e90b0f
Sarah Geerling <tehsarah@gmail.com>
2023-07-11 10:23:20 -0500

    finished exercises

---
+++ part1/exercises/src/components/HobbyLinks.js
@@ -0,0 +1,10 @@
+export default function HobbyLinks () {
+    let hobbyLinks = ["https://www.candlemaking.com/", "https://midwestfragranceco.com/", "https://makesy.com/"];
+    return (
+        <div>
+        <a href = {hobbyLinks[0]}>Aztec Candle and Soap Making Supplies</a><br></br>
+        <a href = {hobbyLinks[1]}>Midwest Fragrance Company</a><br></br>
+        <a href = {hobbyLinks[2]}>Makesy</a><br></br>
+        </div>
+    );
+};
