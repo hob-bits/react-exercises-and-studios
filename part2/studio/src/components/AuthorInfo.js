5e51107099ae9dd0a3f4d4487ad45520f8ab54b1
Sarah Geerling <tehsarah@gmail.com>
2023-07-31 10:30:19 -0500

    finished studio

--- part2/studio/src/components/AuthorInfo.js
+++ part2/studio/src/components/AuthorInfo.js
@@ -1,11 +1,32 @@
+import "./styling.css";
+import recipedata from "./recipe.json";
+
 function AuthorInfo() {
-   return;
- }
+    const recipeAuthor = recipedata.map((data) => (
+      <div key={data.name}>{data.author}</div>
+    ));
+
+    const recipeAuthorImage = recipedata.map((data) => (
+      <div key={data.name}>
+         <img src={data.authorImage} alt={data.author} className="authorImage" />
+      </div>
+    ));
+
+    const recipeWebsite = recipedata.map((data) => (
+      <div key={data.name}>
+        <a href={data.website} />
+      </div>
+    ));
+
+    return (
+      <div>
+         {recipeAuthorImage}
+         {recipeAuthor}
+         {recipeWebsite}
+      </div>
+   );
+
+
+  }
  
  export default AuthorInfo;
- 
- //import styles.css
- //import json file for author info
- //image, name, website
- //css for styling image
- 
--- part2/studio/src/components/RateARecipe.js
+++ part2/studio/src/components/RateARecipe.js
@@ -1,7 +1,13 @@
 let stars = ["⭐", "⭐ ⭐", "⭐ ⭐ ⭐", "⭐ ⭐ ⭐ ⭐", "⭐ ⭐ ⭐ ⭐ ⭐"];
 
-function RateARecipe() {
-  return stars;
+function RateARecipe(props) {
+  function GiveRating() {
+    return <h3>{stars[props.rating - 1]}</h3>;
+ }
+
+ return (
+  (props > 0 && props < 6) ?  <GiveRating /> : null
+ );
 }
 
 export default RateARecipe;
